c6afee45610419b1b1f9a5345351683525aa14ba
Allan Veras <allanverasce@gmail.com>
2024-09-15 18:31:51 -0300

    Update comandos_basicos.sh

--- comandos_basicos/exemplos/comandos_basicos.sh
+++ comandos_basicos/exemplos/comandos_basicos.sh
@@ -1,1 +1,23 @@
 
+#!/bin/bash
+# Script com exemplos de comandos básicos
+
+# Listar arquivos no diretório atual
+echo "Listando arquivos no diretório atual:"
+ls
+
+# Navegar para um diretório específico
+echo "Mudando para o diretório /home:"
+cd /home
+
+# Criar um novo diretório
+echo "Criando o diretório 'novos_projetos':"
+mkdir novos_projetos
+
+# Cria um arquivo chamado arquivo.txt
+echo "Dance, Dance" > arquivo.txt
+
+# Exibir o conteúdo de um arquivo
+echo "Exibindo o conteúdo do arquivo 'arquivo.txt':"
+cat arquivo.txt
+
