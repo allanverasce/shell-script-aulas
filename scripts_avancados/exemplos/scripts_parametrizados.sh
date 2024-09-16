f5b2314ec1bdb614dc198777794a062a526fe554
Allan Veras <allanverasce@gmail.com>
2024-09-16 19:36:08 -0300

    Update scripts_parametrizados.sh

--- scripts_avancados/exemplos/scripts_parametrizados.sh
+++ scripts_avancados/exemplos/scripts_parametrizados.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+
+# Script que recebe o arquivo FASTA e o banco de dados como parâmetros para rodar BLAST
+if [ "$#" -ne 2 ]; then
+  echo "Uso: $0 <arquivo_fasta> <banco_dados>"
+  exit 1
+fi
+
+arquivo_fasta=$1
+banco_dados=$2
+
+# Comando para executar BLAST
+blastn -query "$arquivo_fasta" -db "$banco_dados" -out resultado_blast.txt
+
+echo "BLAST finalizado. Resultado salvo em resultado_blast.txt."
 
