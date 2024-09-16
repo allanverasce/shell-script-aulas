d1d97322b256093acdd318011891a98c85922a4f
Allan Veras <allanverasce@gmail.com>
2024-09-16 18:38:50 -0300

    Update RemoverArquivosAntigos.sh

--- manipulacao_de_arquivos/exemplos/RemoverArquivosAntigos.sh
+++ manipulacao_de_arquivos/exemplos/RemoverArquivosAntigos.sh
@@ -1,1 +1,13 @@
+#Esse script remove arquivos mais antigos que um número específico de dias. Útil para limpar arquivos temporários ou antigos que não são mais necessários.
+#Cuidado no uso... Apagou, apagou KKK
+
+#!/bin/bash
+
+# Diretório contendo os arquivos temporários
+diretorio="/caminho/para/temporarios"
+
+# Remove arquivos com mais de 30 dias
+find "$diretorio" -type f -mtime +30 -exec rm {} \;
+
+echo "Arquivos antigos removidos com sucesso!"
 
