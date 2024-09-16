72a85d9661278a27dd745b0117c67ebeddd7376b
Allan Veras <allanverasce@gmail.com>
2024-09-16 18:29:02 -0300

    Update redirecionamento.sh

--- manipulacao_de_arquivos/exemplos/redirecionamento.sh
+++ manipulacao_de_arquivos/exemplos/redirecionamento.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# Exemplo de redirecionamento simples
+# Escrevendo em um arquivo
+echo "Sequência de DNA" > dna.txt
+
+# Anexando mais informações ao arquivo
+echo "ATGCGTACGTAGCTAG" >> dna.txt
+echo "GCTAGCTAGCTAGCTA" >> dna.txt
+
+# Usando redirecionamento de entrada
+# Contando o número de linhas no arquivo
+wc -l < dna.txt
+
+# Filtrando sequências que contêm "ATG" e salvando em outro arquivo
+grep "ATG" dna.txt > sequencias_com_ATG.txt
 
