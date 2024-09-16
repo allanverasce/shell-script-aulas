b8cd2a03bdebfb6ab1a58965319711f9db868da6
Allan Veras <allanverasce@gmail.com>
2024-09-16 17:41:00 -0300

    Update loops_exemplo.sh

--- fluxo_de_controle/exemplos/loops_exemplo.sh
+++ fluxo_de_controle/exemplos/loops_exemplo.sh
@@ -1,1 +1,9 @@
+# Aqui está um exemplo onde usamos um loop while para processar um arquivo com várias sequências e calcular o comprimento de cada uma.
+#!/bin/bash
 
+while read -r linha; do
+    if [[ "$linha" != ">"* ]]; then
+        comprimento=${#linha}
+        echo "Comprimento da sequência: $comprimento"
+    fi
+done < sequencias.fasta
