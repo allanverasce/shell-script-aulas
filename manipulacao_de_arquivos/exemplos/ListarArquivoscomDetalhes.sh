85d236e2fea67c31c8b3579a1f9c2ec341c814bc
Allan Veras <allanverasce@gmail.com>
2024-09-16 18:40:27 -0300

    Update ListarArquivoscomDetalhes.sh

--- manipulacao_de_arquivos/exemplos/ListarArquivoscomDetalhes.sh
+++ manipulacao_de_arquivos/exemplos/ListarArquivoscomDetalhes.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+# Diretório para listar arquivos
+diretorio="/caminho/para/arquivos"
+
+# Lista os arquivos com detalhes e redireciona para um relatório
+ls -lh "$diretorio" > relatorio_arquivos.txt
+
+echo "Relatório de arquivos gerado em relatorio_arquivos.txt"
 
