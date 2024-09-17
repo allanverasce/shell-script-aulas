51c81f1de0cf921e41918a24b200f358f39901b5
Allan Veras <allanverasce@gmail.com>
2024-09-17 18:27:48 -0300

    Update mapeamentoBWA.sh

--- scripts_avancados/mapeamentoBWA.sh
+++ scripts_avancados/mapeamentoBWA.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+# O seguinte script mapeia leituras usando o BWA e gera um arquivo BAM após o mapeamento.
+
+# Variáveis
+GENOME="genoma.fasta" # Genoma de referência
+READS="leituras.fastq" # Arquivo FASTQ com as leituras de sequenciamento
+OUT="resultado.bam" # Arquivo de saída em formato BAM
+
+# Etapas
+# 1. Indexar o genoma de referência
+bwa index $GENOME
+
+# 2. Mapeamento
+bwa mem $GENOME $READS | samtools view -Sb - > $OUT
+
+# Comentários:
+# O comando `bwa mem` faz o mapeamento das leituras FASTQ ao genoma de referência.
+# O `samtools view -Sb -` converte a saída do mapeamento para o formato BAM.
 
