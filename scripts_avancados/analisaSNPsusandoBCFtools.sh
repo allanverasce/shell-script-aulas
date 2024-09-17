117a40431db9c8fb9cea273e32584b99c50cbdec
Allan Veras <allanverasce@gmail.com>
2024-09-17 18:31:33 -0300

    Update analisaSNPsusandoBCFtools.sh

--- scripts_avancados/analisaSNPsusandoBCFtools.sh
+++ scripts_avancados/analisaSNPsusandoBCFtools.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+# Esse script identifica SNPs em um arquivo BAM e gera um VCF com as variantes.
+
+# Variáveis
+BAM="resultado.bam" # Arquivo BAM gerado após mapeamento
+REFERENCE="genoma.fasta" # Genoma de referência
+VCF_OUT="snps.vcf" # Arquivo de saída VCF com as variantes
+
+# Etapas
+# 1. Gerar um arquivo BCF com as variantes
+bcftools mpileup -f $REFERENCE $BAM | bcftools call -mv -Oz -o $VCF_OUT
+
+# 2. Indexar o arquivo VCF
+bcftools index $VCF_OUT
+
+# Fica atento a explicação dos comandos usados aqui!!!
+# bcftools mpileup: gera a cobertura de bases do BAM em relação ao genoma de referência.
+# bcftools call -mv: chama variantes (SNPs e indels).
+# -Oz: compacta a saída no formato VCF.
 
