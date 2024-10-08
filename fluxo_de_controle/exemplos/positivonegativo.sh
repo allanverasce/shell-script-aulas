39ddb107a8d9cc410680d68725a7329f09a52ba5
Allan Veras <allanverasce@gmail.com>
2024-10-08 18:18:51 -0300

    Update positivonegativo.sh

--- fluxo_de_controle/exemplos/positivonegativo.sh
+++ fluxo_de_controle/exemplos/positivonegativo.sh
@@ -1,1 +1,14 @@
+#Neste exemplo, criamos um script que recebe um número como argumento e verifica se é positivo, negativo ou zero.
 
+#!/bin/bash
+# Recebe o número como argumento
+num=$1
+
+# Verifica se o número é maior, menor ou igual a zero
+if [ $num -gt 0 ]; then
+    echo "$num é um número positivo"
+elif [ $num -lt 0 ]; then
+    echo "$num é um número negativo"
+else
+    echo "$num é zero"
+fi
